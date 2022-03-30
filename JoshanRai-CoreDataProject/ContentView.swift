a6ecaaa6deadea5bf8bb838a1adc6d823042088b
Joshan Rai <pradheon@gmail.com>
2022-03-30 14:56:08 -0700

    Initial Commit

---
+++ JoshanRai-CoreDataProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JoshanRai-CoreDataProject
+//
+//  Created by Joshan Rai on 3/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JoshanRai-CoreDataProject/JoshanRai_CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_CoreDataProjectApp.swift
+//  JoshanRai-CoreDataProject
+//
+//  Created by Joshan Rai on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
