47b25cbccf55f8aa46b27670d5ddb54e9d7044c2
Joshan Rai <pradheon@gmail.com>
2022-03-30 20:36:25 -0700

    v0.4.0
    - Ensuring Core Data objects are unique using constraints
    - Attribute: https://www.hackingwithswift.com/books/ios-swiftui/ensuring-core-data-objects-are-unique-using-constraints
    - New Entity "Wizard" added to Core Data Model.

--- JoshanRai-CoreDataProject/ContentView.swift
+++ JoshanRai-CoreDataProject/ContentView.swift
@@ -6,17 +6,22 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct Student: Hashable {
     let name: String
 }
 
 struct ContentView: View {
-    //   Conditional saving of NSManagedObjectContext
-    @Environment(\.managedObjectContext) var moc
-    
     //   Why does \.self work for ForEach?
     //let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
+    
+    //   Conditional saving of NSManagedObjectContext
+    //@Environment(\.managedObjectContext) var moc
+    
+    //   Ensuring Core Data objects are unique using constraints
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
     
     var body: some View {
         //   Why does \.self work for ForEach?
@@ -33,9 +38,31 @@ struct ContentView: View {
          */
         
         //   Conditional saving of NSManagedObjectContext
+        /*
         Button("Save") {
             if moc.hasChanges {
                 try? moc.save()
+            }
+        }
+         */
+        
+        //   Ensuring Core Data objects are unique using constraints
+        VStack {
+            List(wizards, id: \.self) { wizard in
+                Text(wizard.name ?? "Unknown")
+            }
+            
+            Button("Add") {
+                let wizard = Wizard(context: moc)
+                wizard.name = "harry Potter"
+            }
+            
+            Button("Save") {
+                do {
+                    try moc.save()
+                } catch {
+                    print(error.localizedDescription)
+                }
             }
         }
     }
